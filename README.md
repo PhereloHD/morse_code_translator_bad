100978f006c8eeeeca03dee1fd17bb9282c35111
rnb <rnbwastaken@tutanota.com>
2021-03-10 12:30:07 +0100

    Edited README Fixed the Code and cleaned it up

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # morse_code_translator_bad
-This is a Morse Code Translator that can only translate 1 letter
+This is a simple Morse code Translator written in C++
--- morse_code_translator.cpp
+++ morse_code_translator.cpp
@@ -2,41 +2,30 @@
 #include <string>
 #include <algorithm>
 
-
 int main(){
 
-
-
-
-/* this is the ascii/morse stuff */
-char text[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	char text[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
  '1','2','3','4','5','6','7','8', '9','0' };
- 
-std::string morse[36] = {".-","-...","-.-.","-..",".","..-","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
+
+	std::string morse[36] = {".-","-...","-.-.","-..",".","..-","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
 ".----","..---","...--","....-",".....","-....","--....","---..","----.","-----"};
-/* the algorithm and user input */
 
-std::string userInput;
-std::cout<<"What do you want to translate? \n";
-std::cin>>userInput;
+	std::string userInput;
+	std::cout << "What do you want to translate? \n";
+	std::cin >> userInput;
 
+	std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
+	std::cout << "Your text translated: ";
 
-std::transform(userInput.begin(), userInput.end(), userInput.begin(), ::toupper);
+	for (int i = 0; i < userInput.length(); ++i){
+		for (int counter = 0; counter < 36; counter++){
+			if (userInput.at(i) == text[counter]){
+    				std::cout << " " << morse[counter];
 
-std::cout<<"Your text translated: \n";
-
-for (int i = 0; i<userInput.length(); ++i){
-
-for (int counter = 0; counter < 36; counter++){
-if (userInput.at(i) == text[counter]){
-    std::cout << morse[counter];
-
+			}
+		}
+	}
+	std::cout << "\n";
+	return 0;
 }
-}
-}
-
-    return 0;
-}
-//this is bad
-
-
+//Code "fixed" by rnbunker )))
